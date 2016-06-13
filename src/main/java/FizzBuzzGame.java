a77fbc544fe3c205eb8b73e49e421ce0d79991f4
xiaoqi05 <772508341@qq.com>
2016-06-13 22:16:57 +0800

    add FizzBuzz game implement

--- src/main/java/FizzBuzzGame.java
+++ src/main/java/FizzBuzzGame.java
@@ -1,10 +1,36 @@
 class FizzBuzzGame {
+    private int[] numbers;
+
+    FizzBuzzGame() {
+        this.numbers = new int[100];
+        for (int i = 0; i < 100; i++) {
+            numbers[i] = i + 1;
+        }
+    }
 
     private void print(String content) {
         System.out.println(content);
     }
 
+
     void fizzBuzz() {
-        print("Fizz");
+        for (int i : numbers) {
+            print(sayNumber(i));
+        }
+    }
+
+    String sayNumber(int i) {
+        String result;
+        if (i % 15 == 0) {
+            result = "FizzBuzz";
+        } else if (i % 3 == 0) {
+            result = "Fizz";
+        } else if (i % 5 == 0) {
+            result = "Buzz";
+        } else {
+            result = i + "";
+        }
+
+        return result;
     }
 }
--- src/test/java/FizzBuzzGameTest.java
+++ src/test/java/FizzBuzzGameTest.java
@@ -1,36 +1,35 @@
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzGameTest {
-    private PrintStream console = null;
-    private ByteArrayOutputStream bytes = null;
     private FizzBuzzGame fizzBuzzGame;
 
     @Before
     public void setUp() {
-        bytes = new ByteArrayOutputStream();
-        console = System.out;
-        System.setOut(new PrintStream(bytes));
         fizzBuzzGame = new FizzBuzzGame();
     }
 
-    @After
-    public void tearDown() {
-        System.setOut(console);
-    }
 
     @Test
     public void shouldPrintFizzWhenNumberIsOnlyDivisibleByThree() throws Exception {
-        //asterisk.print("hello world");
-        fizzBuzzGame.fizzBuzz();
-        String s = "Fizz\n";
-        assertEquals(s, bytes.toString());
+        String result = fizzBuzzGame.sayNumber(3);
+        String s = "Fizz";
+        assertEquals(s, result);
     }
 
+    @Test
+    public void shouldPrintBuzzWhenNumberIsOnlyDivisibleByThree() throws Exception {
+        String result = fizzBuzzGame.sayNumber(5);
+        String s = "Buzz";
+        assertEquals(s, result);
+    }
+
+    @Test
+    public void shouldPrintFizzBuzzWhenNumberIsOnlyDivisibleByThree() throws Exception {
+        String result = fizzBuzzGame.sayNumber(15);
+        String s = "FizzBuzz";
+        assertEquals(s, result);
+    }
 }
